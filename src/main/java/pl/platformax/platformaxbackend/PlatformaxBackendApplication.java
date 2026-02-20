2c1fb14661d9ca58a0775b44bc72272a5228661a
Jan Szeliga <janszeliga25@gmail.com>
2026-02-20 22:06:47 +0100

    Initial Spring Boot backend setup

---
+++ src/main/java/pl/platformax/platformaxbackend/PlatformaxBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.platformax.platformaxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformaxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformaxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/platformax/platformaxbackend/PlatformaxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.platformax.platformaxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformaxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
